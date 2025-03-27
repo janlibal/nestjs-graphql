5d7cec82dfda3790c6ef5f9579823a1c0fa1d6ee
Jan Libal <libalj@seznam.cz>
2025-03-27 19:22:01 +0100

    Add validation pipes tests

--- src/auth/__tests__/auth.resolver.test.ts
+++ src/auth/__tests__/auth.resolver.test.ts
@@ -14,6 +14,8 @@ import {
   mockUser,
   registerInput,
 } from './mock/auth.data'
+import { ValidateRegisterPipe } from '../pipes/validate-register.pipe'
+import { BadRequestException } from '@nestjs/common'
 
 const mockAuthService = {
   register: vi.fn(),
@@ -65,6 +67,163 @@ describe('AuthResolver', () => {
         mockAuthService.register.mockResolvedValue(true)
         await authResolver.register(registerInput)
         expect(mockAuthService.register).toHaveBeenCalledWith(registerInput)
+      })
+      it('should throw error when firstName is empty', async () => {
+        const pipe = new ValidateRegisterPipe()
+        try {
+          await pipe.transform({ ...registerInput, firstName: '' }),
+            { type: 'body' }
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(BadRequestException)
+          expect(err).toMatch(
+            /BadRequestException: Firstname cannot be empty and must be a string. Firstname must be longer than 1 character./i,
+          )
+        }
+      })
+      it('should throw error when firstName less than 2 characters long', async () => {
+        const pipe = new ValidateRegisterPipe()
+        try {
+          await pipe.transform({ ...registerInput, firstName: 'J' }),
+            { type: 'body' }
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(BadRequestException)
+          expect(err).toMatch(/Firstname must be longer than 1 character./i)
+        }
+      })
+      it('should throw error when firstName contains special characters', async () => {
+        const pipe = new ValidateRegisterPipe()
+        try {
+          await pipe.transform({ ...registerInput, firstName: '@#@#' }),
+            { type: 'body' }
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(BadRequestException)
+          expect(err).toMatch(
+            /Firstname cannot contain special characters or numbers./i,
+          )
+        }
+      })
+      it('should throw error when firstName consists of only numbers', async () => {
+        const pipe = new ValidateRegisterPipe()
+        try {
+          await pipe.transform({ ...registerInput, firstName: '123' }),
+            { type: 'body' }
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(BadRequestException)
+          expect(err).toMatch(
+            /Firstname cannot contain special characters or numbers./i,
+          )
+        }
+      })
+      it('should throw error when lastName is empty', async () => {
+        const pipe = new ValidateRegisterPipe()
+        try {
+          await pipe.transform({ ...registerInput, lastName: '' }),
+            { type: 'body' }
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(BadRequestException)
+          expect(err).toMatch(/Lastname cannot be empty and must be a string./i)
+        }
+      })
+      it('should throw error when lastName is empty', async () => {
+        const pipe = new ValidateRegisterPipe()
+        try {
+          await pipe.transform({ ...registerInput, lastName: '' }),
+            { type: 'body' }
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(BadRequestException)
+          expect(err).toMatch(/Lastname cannot be empty and must be a string./i)
+        }
+      })
+      it('should throw error when lastName less than 2 characters long', async () => {
+        const pipe = new ValidateRegisterPipe()
+        try {
+          await pipe.transform({ ...registerInput, lastName: 'D' }),
+            { type: 'body' }
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(BadRequestException)
+          expect(err).toMatch(/Lastname must be longer than 1 character./i)
+        }
+      })
+
+      it('should throw error when lastName contains special characters', async () => {
+        const pipe = new ValidateRegisterPipe()
+        try {
+          await pipe.transform({ ...registerInput, lastName: '@#@#' }),
+            { type: 'body' }
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(BadRequestException)
+          expect(err).toMatch(
+            /Lastname cannot contain special characters or numbers./i,
+          )
+        }
+      })
+      it('should throw error when lastName consists of only numbers', async () => {
+        const pipe = new ValidateRegisterPipe()
+        try {
+          await pipe.transform({ ...registerInput, lastName: '123' }),
+            { type: 'body' }
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(BadRequestException)
+          expect(err).toMatch(
+            /Lastname cannot contain special characters or numbers./i,
+          )
+        }
+      })
+      it('should throw error when password is empty', async () => {
+        const pipe = new ValidateRegisterPipe()
+        try {
+          await pipe.transform({ ...registerInput, password: '' }),
+            { type: 'body' }
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(BadRequestException)
+          expect(err).toMatch(/Password cannot be empty./i)
+        }
+      })
+      it('should throw error when password is less than 6 characters long', async () => {
+        const pipe = new ValidateRegisterPipe()
+        try {
+          await pipe.transform({ ...registerInput, password: 'Pas' }),
+            { type: 'body' }
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(BadRequestException)
+          expect(err).toMatch(/Password must be at least 6 characters long./i)
+        }
+      })
+      it('should throw error when password is more than 20 characters long', async () => {
+        const pipe = new ValidateRegisterPipe()
+        try {
+          await pipe.transform({
+            ...registerInput,
+            password:
+              'Password123!Password123!Password123!Password123!Password123!Password123!',
+          }),
+            { type: 'body' }
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(BadRequestException)
+          expect(err).toMatch(
+            /Passwword can contain 20 characters at the most./i,
+          )
+        }
+      })
+      it('should throw error when password is weak', async () => {
+        const pipe = new ValidateRegisterPipe()
+        try {
+          await pipe.transform({ ...registerInput, password: 'Pas' }),
+            { type: 'body' }
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(BadRequestException)
+          expect(err).toMatch(/Password is too weak/i)
+        }
+      })
+      it('should throw error when email is invalid', async () => {
+        const pipe = new ValidateRegisterPipe()
+        try {
+          await pipe.transform({ ...registerInput, email: 'aa@bb' }),
+            { type: 'body' }
+        } catch (err: any) {
+          expect(err).toBeInstanceOf(BadRequestException)
+          expect(err).toMatch(/Email is invalid./i)
+        }
       })
     })
     describe('login()', () => {
