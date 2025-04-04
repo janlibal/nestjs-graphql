7e410d00a1046ea76b5c8f26b91fd8c5a9f5887a
Jan Libal <libalj@seznam.cz>
2025-04-04 13:57:39 +0200

    style: format all files

--- src/interceptors/graphql-logging.interceptor.ts
+++ src/interceptors/graphql-logging.interceptor.ts
@@ -1,36 +1,35 @@
-import { Injectable } from '@nestjs/common';
-import { ExecutionContext, CallHandler, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { Logger } from 'nestjs-pino';
-import { GqlExecutionContext } from '@nestjs/graphql';
-import { PinoLoggerService } from '../logger/adapters/pino.logger.service';
+import { Injectable } from '@nestjs/common'
+import { ExecutionContext, CallHandler, NestInterceptor } from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { tap } from 'rxjs/operators'
+import { Logger } from 'nestjs-pino'
+import { GqlExecutionContext } from '@nestjs/graphql'
+import { PinoLoggerService } from '../logger/adapters/pino.logger.service'
 
 @Injectable()
 export class GraphqlLoggingInterceptor implements NestInterceptor {
   constructor(private readonly logger: PinoLoggerService) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const gqlContext = GqlExecutionContext.create(context);
-    const { variables } = gqlContext.getArgs();
-    const operationType = gqlContext.getContext().operationName ? 'Query' : 'Mutation'; 
-    const operationName = gqlContext.getContext().req.body.operationName || 'Unnamed operation';
+    const gqlContext = GqlExecutionContext.create(context)
+    const { variables } = gqlContext.getArgs()
+    const operationType = gqlContext.getContext().operationName
+      ? 'Query'
+      : 'Mutation'
+    const operationName =
+      gqlContext.getContext().req.body.operationName || 'Unnamed operation'
 
     this.logger.log(
       { operationName: operationName, variables },
       `Incoming GraphQL ${operationType}: ${operationName}`,
-    );
+    )
 
-    console.log('GQL Context:', gqlContext.getContext());
-
+    console.log('GQL Context:', gqlContext.getContext())
 
     return next.handle().pipe(
       tap((result) => {
-        this.logger.log(
-          { result },
-          `GraphQL Response for ${operationName}`,
-        );
+        this.logger.log({ result }, `GraphQL Response for ${operationName}`)
       }),
-    );
+    )
   }
 }
--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -3,7 +3,7 @@ import { LoggerModule as PinoLoggerModule } from 'nestjs-pino'
 import * as uuid from 'uuid'
 import { FakeLoggerService } from './adapters/fake.logger.service'
 import { PinoLoggerService } from './adapters/pino.logger.service'
-import * as rfs from 'rotating-file-stream';
+import * as rfs from 'rotating-file-stream'
 
 declare module 'http' {
   interface IncomingMessage {
@@ -19,24 +19,25 @@ declare module 'http' {
         name: 'backend',
         level: process.env.NODE_ENV !== 'prod' ? 'debug' : 'info',
         genReqId: (req) => req.requestId || uuid.v4(),
-        transport: process.env.NODE_ENV !== 'production'
-          ? {
-              target: 'pino-pretty',
-              options: {
-                colorize: true,
-                translateTime: 'SYS:standard',
-                ignorePaths: ['pid', 'hostname'],
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? {
+                target: 'pino-pretty',
+                options: {
+                  colorize: true,
+                  translateTime: 'SYS:standard',
+                  ignorePaths: ['pid', 'hostname'],
+                },
+              }
+            : {
+                target: 'pino/file',
+                options: {
+                  destination: rfs.createStream('app-%DATE%.log', {
+                    interval: '1d',
+                    path: './logs',
+                  }),
+                },
               },
-            }
-          : {
-              target: 'pino/file',
-              options: {
-                destination: rfs.createStream('app-%DATE%.log', {
-                  interval: '1d', 
-                  path: './logs', 
-                }),
-              },
-            },
       },
     }),
   ],
