75f47f8f61ab46f086bc848e3a9a070c0a51bf46
unknown <knoflik21@seznam.cz>
2025-04-17 13:34:04 +0200

    refactor: update mock data

--- tests/helpers/mock.data.ts
+++ tests/helpers/mock.data.ts
@@ -3,7 +3,7 @@ import crypto from '../../src/utils/crypto'
 export const usr = {
   firstName: 'Daniel',
   lastName: 'Doe',
-  password: await crypto.hashPassword('Password123!'),
+  password: crypto.hashPassword('Password123!'),
   email: 'daniel.doe@joedoe.com',
   statusId: 1,
   roleId: 1
