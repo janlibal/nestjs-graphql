b8bc7a700632eedaf9f76e0d0b9adb4a5ea7f8ff
Jan Libal <libalj@seznam.cz>
2025-04-18 08:52:34 +0200

    chore: update relative paths

--- src/app/app.resolver.ts
+++ src/app/app.resolver.ts
@@ -2,7 +2,7 @@ import { HelloResponseDto } from './dto/hello-response.dto'
 import { Resolver, Query, Args } from '@nestjs/graphql'
 import { ValidateNamePipe } from './pipes/validate-name.pipe'
 import { UseGuards } from '@nestjs/common'
-import { GqlAuthGuard } from 'src/auth/guards/gpl-auth.guard'
+import { GqlAuthGuard } from '../auth/guards/gpl-auth.guard'
 import { NotFoundException } from '@nestjs/common'
 
 @Resolver('App')
