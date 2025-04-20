3fee674ddcea826ce738a28685c89d803bdfd89e
Jan Libal <libalj@seznam.cz>
2025-04-20 10:20:01 +0200

    chore: update ci dockerfile

--- startup.relational.ci.sh
+++ startup.relational.ci.sh
@@ -3,8 +3,19 @@ set -e
 
 /opt/wait-for-it.sh postgres:5432
 yarn run migrate:deploy
-yarn run seed:prod
+node dist/prisma/seeds/seed.js
+
 yarn run start:prod > prod.log 2>&1 &
 /opt/wait-for-it.sh localhost:80
 /opt/wait-for-graphql.sh http://localhost:80/api/v1/graphql
 yarn run test:e2e:ci
+
+
+
+#/opt/wait-for-it.sh postgres:5432
+#yarn run migrate:deploy
+#yarn run seed:prod
+#yarn run start:prod > prod.log 2>&1 &
+#/opt/wait-for-it.sh localhost:80
+#/opt/wait-for-graphql.sh http://localhost:80/api/v1/graphql
+#yarn run test:e2e:ci
