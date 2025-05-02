b0527ff1d0b1fbe9a4e60c1ec04281c85c136801
unknown <knoflik21@seznam.cz>
2025-05-02 14:28:34 +0200

    chore(ci): add test container

--- startup.relational.ci.sh
+++ startup.relational.ci.sh
@@ -4,7 +4,4 @@ set -e
 /opt/wait-for-it.sh postgres:5432 --timeout=60 --strict
 yarn run migrate:deploy
 yarn run seed:prod
-yarn run start:prod > prod.log 2>&1 &
-/opt/wait-for-it.sh localhost:80 --timeout=60 --strict
-/opt/wait-for-graphql.sh http://localhost:80/api/v1/graphql --timeout=60 --strict
-yarn run test:e2e:ci
+yarn run start:prod
