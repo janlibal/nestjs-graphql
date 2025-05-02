caa01258fd4b66d7277042415ae9df2916e89609
Jan Libal <libalj@seznam.cz>
2025-05-02 19:00:02 +0200

    chore: rollback 2

--- startup.relational.ci.sh
+++ startup.relational.ci.sh
@@ -4,4 +4,7 @@ set -e
 /opt/wait-for-it.sh postgres:5432 --timeout=60 --strict
 yarn run migrate:deploy
 yarn run seed:prod
-yarn run start:prod
+yarn run start:prod > prod.log 2>&1 &
+/opt/wait-for-it.sh localhost:3600 --timeout=60 --strict
+/opt/wait-for-graphql.sh http://localhost:3600/api/v1/graphql
+yarn run test:e2e:ci
