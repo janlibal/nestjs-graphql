7f327c41eb3199a3fa2991c5afb9ae85a97761d1
Jan Libal <libalj@seznam.cz>
2025-12-16 19:39:22 +0100

    fix: ports numbers rollback

--- startup.relational.ci.sh
+++ startup.relational.ci.sh
@@ -5,6 +5,6 @@ set -e
 yarn run migrate:deploy
 yarn run seed:prod
 yarn run start:prod > prod.log 2>&1 &
-/opt/wait-for-it.sh localhost:80 --timeout=60 --strict
+/opt/wait-for-it.sh localhost:3600 --timeout=60 --strict
 /opt/wait-for-graphql.sh http://localhost:3600/api/v1/graphql
 yarn run test:e2e:ci
